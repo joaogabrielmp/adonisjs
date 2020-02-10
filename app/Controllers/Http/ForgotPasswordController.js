615ad715f573d030b585a4935b783496c9bda3e1
joaogabrielmp <joaogabrielma@gmail.com>
2020-02-10 16:54:15 -0300

    send email

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store({ request, response }) {
@@ -13,10 +14,35 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        'emails.forgot_password',
+        {
+          email,
+          token: user.token,
+          link: ` http://www.meusistema.com/resetar_senha?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('jgmp@me.com')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (error) {
       return response
         .status(error.status)
         .send({ error: { message: 'Algo deu errado, esse email existe?' } })
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all()
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Algo deu errado ao resetar a sua senha' } })
     }
   }
 }
