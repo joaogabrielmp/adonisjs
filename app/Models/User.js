07f48404bda253aef19093698276ce14d7fe7ee5
joaogabrielmp <joaogabrielma@gmail.com>
2020-02-11 17:00:22 -0300

    relationships

--- app/Models/User.js
+++ app/Models/User.js
@@ -18,6 +18,14 @@ class User extends Model {
   tokens() {
     return this.hasMany('App/Models/Token')
   }
+
+  projects() {
+    return this.hasMany('App/Models/Projct')
+  }
+
+  tasks() {
+    return this.hasMany('App/Models/Task')
+  }
 }
 
 module.exports = User
